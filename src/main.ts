cc8ddc0e692ec491ae2f9fece9ac32eaa412b127
shenoyranjith <shenoyranjith95@gmail.com>
2023-03-28 14:15:27 +0530

    🔧 Update test data

--- src/main.ts
+++ src/main.ts
@@ -2,29 +2,29 @@ import FamilyTree, { Node } from './FamilyTree';
 
 const root = {
   id: 1,
-  name: 'Mukundarao',
+  name: 'Test1',
   image:
     'https://png.pngtree.com/png-vector/20220709/ourmid/pngtree-businessman-user-avatar-wearing-suit-with-red-tie-png-image_5809521.png',
   generation: 0,
   partners: [
     {
       id: 2,
-      name: 'Shyamala',
+      name: 'Test2',
       isMarried: true,
       children: [
         {
           id: 3,
-          name: 'Indira',
+          name: 'Test3',
           generation: 1,
           partners: [
             {
-              id: 5,
-              name: 'Sreedhar',
+              id: 4,
+              name: 'Test4',
               isMarried: true,
               children: [
                 {
-                  id: 6,
-                  name: 'Sindhu',
+                  id: 5,
+                  name: 'Test5',
                   generation: 2,
                 },
               ],
@@ -32,23 +32,23 @@ const root = {
           ],
         },
         {
-          id: 7,
-          name: 'Ramakrishna',
+          id: 6,
+          name: 'Test6',
           generation: 1,
           partners: [
             {
-              id: 8,
-              name: 'Jayasree',
+              id: 7,
+              name: 'Test7',
               isMarried: true,
               children: [
                 {
-                  id: 9,
-                  name: 'Ranjith',
+                  id: 8,
+                  name: 'Test8',
                   generation: 2,
                   partners: [
                     {
-                      id: 10,
-                      name: 'Shilpa',
+                      id: 9,
+                      name: 'Test9',
                       isMarried: true,
                     },
                   ],
@@ -58,28 +58,28 @@ const root = {
           ],
         },
         {
-          id: 11,
-          name: 'Ramadevi',
+          id: 10,
+          name: 'Test10',
           generation: 1,
           partners: [
             {
-              id: 12,
-              name: 'Gopinath',
+              id: 11,
+              name: 'Test11',
               isMarried: true,
               children: [
                 {
-                  id: 13,
-                  name: 'Reshmi',
+                  id: 12,
+                  name: 'Test12',
                   generation: 2,
                   partners: [
                     {
-                      id: 14,
-                      name: 'Raj',
+                      id: 13,
+                      name: 'Test13',
                       isMarried: true,
                       children: [
                         {
-                          id: 15,
-                          name: 'Pratham',
+                          id: 14,
+                          name: 'Test14',
                           generation: 3,
                         },
                       ],
@@ -91,40 +91,40 @@ const root = {
           ],
         },
         {
-          id: 16,
-          name: 'Padmini',
+          id: 15,
+          name: 'Test15',
           generation: 1,
           partners: [
             {
-              id: 17,
-              name: 'Rajesh',
+              id: 16,
+              name: 'Test16',
               isMarried: true,
               children: [
                 {
-                  id: 18,
-                  name: 'Sowmya',
+                  id: 17,
+                  name: 'Test17',
                   generation: 2,
                   partners: [
                     {
-                      id: 19,
-                      name: 'Arjun',
+                      id: 18,
+                      name: 'Test18',
                       isMarried: true,
                     },
                   ],
                 },
                 {
-                  id: 20,
-                  name: 'Swathi',
+                  id: 19,
+                  name: 'Test19',
                   generation: 2,
                   partners: [
                     {
-                      id: 21,
-                      name: 'Pradeep',
+                      id: 20,
+                      name: 'Test20',
                       isMarried: true,
                       children: [
                         {
-                          id: 22,
-                          name: 'Trishika',
+                          id: 21,
+                          name: 'Test21',
                           generation: 3,
                         },
                       ],
@@ -136,18 +136,18 @@ const root = {
           ],
         },
         {
-          id: 23,
-          name: 'Rajalakshmi',
+          id: 22,
+          name: 'Test22',
           generation: 1,
           partners: [
             {
-              id: 24,
-              name: 'Sridhar',
+              id: 23,
+              name: 'Test23',
               isMarried: true,
               children: [
                 {
-                  id: 25,
-                  name: 'Rithwik',
+                  id: 24,
+                  name: 'Test24',
                   generation: 2,
                 },
               ],
